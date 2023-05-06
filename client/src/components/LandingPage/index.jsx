cb714e755ac14e75e565c1c36e84fdd6b0fd3afe
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2023-05-06 16:52:24 +0500

    Added Landing Page

---
+++ client/src/components/LandingPage/index.jsx
@@ -0,0 +1,12 @@
+
+import Navbar from './Navbar'
+import HeroSection from './HeroSection'
+
+export default function index() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection/>
+    </>
+  )
+}
