5e9b7d299a9c885289d3752d68a809034ee07697
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2023-05-08 23:00:10 +0500

    created a proper register and login system

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', require('./routes/authRoutes'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- backend/models/User.js
+++ backend/models/User.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username:{
+    email:{
         type:String,
         required:true,
-        unique:true
+        unique:true,
     },
     password:{
         type:String,
