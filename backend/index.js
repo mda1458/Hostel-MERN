67cc7cec479dfc7e7b8d3efe3eae56d99231f993
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2023-05-09 10:24:25 +0500

    included cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const connectDB = require('./utils/conn')
+const cors = require('cors')
 
 const app = express()
 const port = 3000
 
 connectDB();
+
+app.use(cors());
 
 app.use(express.json({ extended: false }));
 
