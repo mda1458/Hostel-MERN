d5d2e31a2ff942d619763b3c15a3d4fa971bffe0
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2023-05-10 23:33:02 +0500

    Fix Error in Student Home

--- client/src/components/Dashboards/StudentDashboard/Home.jsx
+++ client/src/components/Dashboards/StudentDashboard/Home.jsx
@@ -1,5 +1,3 @@
-// import { Doughnut } from "react-chartjs-2";
-// import 'chart.js/auto'; 
 
 function Home() {
   let user = JSON.parse(localStorage.getItem("user"));
@@ -10,7 +8,7 @@ function Home() {
         <h1 className="text-white font-bold text-5xl">
           Welcome <span className="text-blue-700">Student!</span>
         </h1>
-  =======
+      </div>
       <div className="w-full h-screen flex items-center justify-center" >
           <h1 className="text-gray-900 font-bold text-5xl" >Welcome <span className="text-blue-500">{user.email}</span></h1>
       </div>
