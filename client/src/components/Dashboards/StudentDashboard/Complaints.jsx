26f1ec5c2ca78e1c67fdf99ed33f405ef15c74e0
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2023-05-11 14:57:54 +0500

    Added Complaints Page

--- client/src/components/Dashboards/StudentDashboard/Complaints.jsx
+++ client/src/components/Dashboards/StudentDashboard/Complaints.jsx
@@ -1,11 +1,97 @@
+import { useState } from "react";
+import { Input } from "../../LandingSite/AuthPage/Input";
 function Complaints() {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [type, setType] = useState("Electric");
+
+  const types = ["Electric", "Furniture", "Cleaning", "Others"];
+
+  function chngType(e) {
+    setType(e.target.value);
+  }
+
+  function titleChange(e) {
+    setTitle(e.target.value);
+  }
+  function descChange(e) {
+    setDesc(e.target.value);
+  }
+
+  const complaintTitle = {
+    name: "complaint title",
+    placeholder: "Title",
+    req: true,
+    type: "text",
+    value: title,
+    onChange: titleChange,
+  };
+  const complaintType = {
+    name: "complaint type",
+    placeholder: "Type...",
+    req: true,
+    type: "text",
+    value: type,
+    onChange: chngType,
+  };
+
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <h1 className="text-white font-bold text-5xl">
-        Complaints
-      </h1>
+    <div className="w-full h-screen flex flex-col gap-10 items-center justify-center md:p-0 px-10">
+      <h1 className="text-white font-bold text-5xl mt-10">Complaints</h1>
+      <form
+        method="POST"
+        action="#"
+        className="md:w-[30vw] w-full py-5 pb-7 px-10 bg-neutral-950 rounded-lg shadow-xl flex flex-col gap-5"
+      >
+        <div>
+          <label
+            htmlFor="description"
+            className="block mb-2 text-sm font-medium text-white"
+          >
+            Your complaint type
+          </label>
+          <select
+            className="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 outline-none"
+            onChange={chngType}
+          >
+            {types.map((type) => (
+              <option key={type}>{type}</option>
+            ))}
+          </select>
+          {
+            type.toLowerCase() === 'electric' || type.toLowerCase() === 'furniture' || type.toLowerCase() === 'cleaning' ?  
+            <></> : (
+              <div className="mt-5">
+                <Input field={complaintType} />
+              </div>
+              )
+          }
+        </div>
+        <Input field={complaintTitle} />
+        <div>
+          <label
+            htmlFor="description"
+            className="block mb-2 text-sm font-medium text-white"
+          >
+            Your complaint Description
+          </label>
+          <textarea
+            name="description"
+            placeholder="Details of complaint"
+            className="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 outline-none"
+            onChange={descChange}
+            value={desc}
+          ></textarea>
+          <button
+            type="submit"
+            className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-800 text-lg rounded-lg px-5 py-2.5 mt-5 text-center"
+          >
+            Register Complaint
+          </button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Complaints;
--- client/src/components/LandingSite/AuthPage/Input.jsx
+++ client/src/components/LandingSite/AuthPage/Input.jsx
@@ -7,7 +7,7 @@ Input.propTypes = {
       req: PropTypes.bool.isRequired,
       type: PropTypes.string.isRequired,
       onChange: PropTypes.func.isRequired,
-      value: PropTypes.string.isRequired,
+      value: PropTypes.string,
     }).isRequired,
   };
   
@@ -21,7 +21,7 @@ function Input({ field }) {
     return (
         <div>
             <label htmlFor={name} className="block mb-2 text-sm font-medium text-white">Your {name}</label>
-            <input type={type} name={name} id={name} className="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 outline-none" placeholder={placeholder} required={required} onChange={field.onChange} />
+            <input type={type} name={name} id={name} className="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 outline-none" placeholder={placeholder} required={required} value={value} onChange={field.onChange} />
         </div>
     );
 }
