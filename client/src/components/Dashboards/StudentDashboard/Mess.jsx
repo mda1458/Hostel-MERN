c4f176a0199b1368add66b9524de09d06d582a8d
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2023-05-12 17:29:10 +0500

    Updated Student Mess Off Page

--- client/src/components/Dashboards/StudentDashboard/Mess.jsx
+++ client/src/components/Dashboards/StudentDashboard/Mess.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { Input } from "../../LandingSite/AuthPage/Input";
-import { Bar } from "react-chartjs-2";
+import { Doughnut } from "react-chartjs-2";
 import "chart.js/auto"; // !IMPORTANT
 
 function Mess() {
   let requestMessOff = async (event) => {
     event.preventDefault();
+    setLoading(true);
     let data = {
       student: JSON.parse(localStorage.getItem("student"))._id,
       leaving_date: leaveDate,
@@ -30,6 +31,8 @@ function Mess() {
     } else {
       alert(result.message);
     }
+
+    setLoading(false);
   };
 
   let daysofmonthtilltoday = new Date().getDate();
@@ -38,6 +41,21 @@ function Mess() {
   const [returnDate, setReturnDate] = useState("");
   const [requests, setRequests] = useState(0);
   const [mealsoff, setMealsOff] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [requestsList, setRequestsList] = useState([
+    {
+      reqDate: '12-5-2023',
+      leavingDate: '13-5-2023',
+      returnDate: '17-5-2023',
+      status: 'Pending'
+    },
+    {
+      reqDate: '12-5-2023',
+      leavingDate: '13-5-2023',
+      returnDate: '17-5-2023',
+      status: 'Pending'
+    },
+  ]);
 
   function handleleaveChange(e) {
     setLeaveDate(e.target.value);
@@ -67,6 +85,7 @@ function Mess() {
 
   useEffect(() => {
     let student = JSON.parse(localStorage.getItem("student"));
+    setLoading(true);
     if (student) {
       fetch("http://localhost:3000/api/messoff/count", {
         method: "POST",
@@ -77,9 +96,7 @@ function Mess() {
           student: student._id,
         }),
       })
-        .then((res) => {
-          res.json()
-        })
+        .then((res) => res.json())
         .then((result) => {
           if (result.success) {
             setMealsOff(result.approved);
@@ -89,38 +106,111 @@ function Mess() {
           }
         });
     }
+    setLoading(false);
   }, [requests]);
 
-            
-
-  
-  const labels =  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  const labels = ["Meals Off", "Requested Meals Off", "Meals Attended"];
+  const loader = (
+    <svg
+      aria-hidden="true"
+      className="inline w-4 h-4 mr-2 animate-spin text-white fill-blue-600"
+      viewBox="0 0 100 101"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+        fill="currentColor"
+      />
+      <path
+        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+        fill="currentFill"
+      />
+    </svg>
+  );
 
   return (
     <div className="w-full h-screen pt-20 gap-10 flex flex-col items-center justify-center">
       <h1 className="text-white font-bold text-5xl">Mess</h1>
       <ul className="flex gap-5 text-white text-xl">
-        <li>Total Meals: {daysofmonthtilltoday*3}</li>
-        <li>Meals Off: {mealsoff}</li>
-        <li>Requests Sent: {requests}</li>
+        <li>Total Meals: {daysofmonthtilltoday - mealsoff} </li>
+        <li>Meals Off: {loading ? loader : mealsoff}</li>
+        <li>Requests Sent: {loading ? loader : requests}</li>
       </ul>
-      <div className="w-full h-[30vh] flex flex-col items-center justify-center">
-        <span className="text-white text-lg mb-2">This Week</span>
-        <Bar
-          datasetIdKey="id"
-          data={{
-            labels,
-            datasets: [
-              {
-                label: "No. of Meals",
-                data: [3, 2, 2, 3, 2, 3, 1],
-                backgroundColor: '#1D4ED8',
-                barThickness: 40,
-                borderRadius: 5,
+      <div className="w-full h-[30vh] gap-10 flex items-center justify-center">
+        <div className="h-[30vh] gap-2 flex items-center justify-center">
+          <Doughnut
+            datasetIdKey="id"
+            data={{
+              labels,
+              datasets: [
+                {
+                  label: "meals",
+                  data: [mealsoff, requests, daysofmonthtilltoday-mealsoff],
+                  backgroundColor: ["#F26916", "#EAB308", "#1D4ED8"],
+                  barThickness: 20,
+                  borderRadius: 0,
+                  borderJoinStyle: "round",
+                  borderColor: "rgba(0,0,0,0)",
+                  hoverOffset: 10,
+                },
+              ],
+            }}
+            options={{
+              plugins: {
+                legend: {
+                  display: false,
+                },
               },
-            ],
-          }}
-        />
+            }}
+          />
+          <ul className="text-white">
+            <li className="flex gap-2">
+              {" "}
+              <span className="w-10 h-5 bg-orange-500 block"></span> Meals Off
+            </li>
+            <li className="flex gap-2">
+              {" "}
+              <span className="w-10 h-5 bg-yellow-500 block"></span> Requested
+              Meals
+            </li>
+            <li className="flex gap-2">
+              {" "}
+              <span className="w-10 h-5 bg-blue-500 block"></span> Meals
+              Attended
+            </li>
+          </ul>
+        </div>
+        <div className="w-80 max-w-md max-h-60 p-4 border rounded-lg shadow sm:p-8 bg-neutral-950 border-neutral-900 drop-shadow-xl overflow-y-auto">
+          <div className="flex items-center justify-between mb-4">
+            <h5 className="text-xl font-bold leading-none text-white">
+              All Requests
+            </h5>
+          </div>
+          <div className="flow-root">
+            <ul role="list" className="divide-y divide-gray-700 text-white ">
+              {requestsList.length === 0
+                ? "No requests Sent"
+                : requestsList.map((req) => (
+                    <li className="py-3 sm:py-4" key={req.reqDate}>
+                      <div className="flex items-center space-x-4">
+                        <div className="flex-1 min-w-0">
+                          <p className="text-sm font-medium truncate text-white">
+                            {req.status}
+                          </p>
+                          <p className="text-sm truncate text-gray-400">
+                            {req.leavingDate} to {req.returnDate}
+                          </p>
+                        </div>
+                        <div className="flex flex-col items-center text-base font-semibold text-white">
+                          {req.reqDate}
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+            </ul>
+          </div>
+        </div>
       </div>
       <form
         method="POST"
@@ -135,7 +225,11 @@ function Mess() {
           type="submit"
           className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-800 text-xl rounded-lg px-5 py-2.5 mt-5 text-center"
         >
-          Request Mess off
+          {loading ? (
+            <div>{loader} Sending Request...</div>
+          ) : (
+            "Request Mess off"
+          )}
         </button>
       </form>
     </div>
