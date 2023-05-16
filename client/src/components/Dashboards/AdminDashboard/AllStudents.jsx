adc0309a5b8d44a5339e72fdda8c3f04ca2a5070
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2023-05-16 22:36:46 +0500

    Add Edit-Del options to All Student Pg

--- client/src/components/Dashboards/AdminDashboard/AllStudents.jsx
+++ client/src/components/Dashboards/AdminDashboard/AllStudents.jsx
@@ -19,8 +19,12 @@ function AllStudents() {
     <div className="w-full h-screen flex flex-col gap-5 items-center justify-center">
       <h1 className="text-white font-bold text-5xl">All Students</h1>
       <div className="w-96 flex justify-center">
-        <a href="../public/paging.pdf" target="_blank" download={true}
-        className="px-20 py-3 bg-blue-700 hover:bg-blue-800 text-white font-bold rounded-xl shadow-xl">
+        <a
+          href="../public/paging.pdf"
+          target="_blank"
+          download={true}
+          className="px-20 py-3 bg-blue-700 hover:bg-blue-800 text-white font-bold rounded-xl shadow-xl"
+        >
           Download List
         </a>
       </div>
@@ -56,9 +60,40 @@ function AllStudents() {
                         {student.cms} | Room: {student.room}
                       </p>
                     </div>
-                    <p className="text-lg font-bold truncate text-white">
-                      {student.hostel}
-                    </p>
+                    <div className="flex gap-3">
+                      <button className="hover:underline">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                          />
+                        </svg>
+                      </button>
+                      <button className="hover:underline ">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                          />
+                        </svg>
+                      </button>
+                    </div>
                   </div>
                 </li>
               ))}
