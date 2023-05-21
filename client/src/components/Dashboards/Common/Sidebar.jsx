3dcf9ff0e7502dc5b781feecfc1d5876bb47f140
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2023-05-21 15:55:02 +0500

    Fix Menu Open Bug

--- client/src/components/Dashboards/Common/Sidebar.jsx
+++ client/src/components/Dashboards/Common/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
@@ -17,13 +17,24 @@ function Sidebar({ links }) {
     localStorage.removeItem("student");
     localStorage.removeItem("token");
     navigate("/");
-}
+  };
   const [isOpen, setIsOpen] = useState(true);
   const location = useLocation();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const setWindowDimensions = () => {
+    setWindowWidth(window.innerWidth);
+    if(window.innerWidth >= 768) {
+      setIsOpen(true);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("resize", setWindowDimensions);
+  }, []);
 
   return (
     <div>
@@ -63,7 +74,10 @@ function Sidebar({ links }) {
           isOpen ? "translate-x-0" : "absolute -translate-x-full"
         }`}
       >
-        <Link to={`/${links[0].for}-dashboard`} className="py-4 px-4 md:py-5 lg:py-4 gap-2 bg-blue-700 flex items-center text-2xl">
+        <Link
+          to={`/${links[0].for}-dashboard`}
+          className="py-4 px-4 md:py-5 lg:py-4 gap-2 bg-blue-700 flex items-center text-2xl"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -77,7 +91,7 @@ function Sidebar({ links }) {
               strokeLinejoin="round"
               d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
             />
-          </svg> {" "}
+          </svg>{" "}
           <span className="md:hidden lg:inline">Dashboard</span>
         </Link>
         <div className="flex flex-col space-y-1 text-2xl text-white">
