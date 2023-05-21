a246a015d54f79d8f2b70379fa921d2f72897b4e
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2023-05-21 23:12:26 +0500

    Created suggestion backend

---
+++ backend/controllers/suggestionController.js
@@ -0,0 +1,70 @@
+const { validationResult } = require('express-validator');
+const { Suggestion } = require('../models');
+
+// @route   Register api/suggestion
+// @desc    Register suggestion
+// @access  Public
+exports.registerSuggestion = async (req, res) => {
+    let success = false;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array(), success });
+    }
+    const { student, hostel, title, description } = req.body;
+    try {
+        const newSuggestion = new Suggestion({
+            student,
+            hostel,
+            title,
+            description
+        });
+        await newSuggestion.save();
+        success = true;
+        res.json({ success, msg: 'Suggestion registered successfully' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+}
+
+// @route   GET api/suggestion
+// @desc    Get all suggestions by hostel id
+// @access  Public
+exports.getbyhostel = async (req, res) => {
+    let success = false;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array(), success });
+    }
+    const { hostel } = req.body;
+    try {
+        const suggestions = await Suggestion.find({ hostel }).populate('student', ['name', 'room_no']);
+        success = true;
+        res.json({ success, suggestions });
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+}
+
+// @route   GET api/suggestion
+// @desc    Get all suggestions by student id
+// @access  Public
+exports.getbystudent = async (req, res) => {
+    let success = false;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array(), success });
+    }
+    const { student } = req.body;
+    try {
+        const suggestions = await Suggestion.find({ student }).populate('hostel', ['name']);
+        success = true;
+        res.json({ success, suggestions });
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+}
---
+++ backend/routes/suggestionRoutes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const { check } = require('express-validator');
+const { registerSuggestion, getbyhostel, getbystudent } = require('../controllers/suggestionController');
+
+// @route   Register api/suggestion
+// @desc    Register suggestion
+// @access  Public
+router.post('/register', [
+    check('student', 'Student is required').not().isEmpty(),
+    check('hostel', 'Hostel is required').not().isEmpty(),
+    check('title', 'Title is required').not().isEmpty(),
+    check('description', 'Description is required').not().isEmpty()
+], registerSuggestion);
+
+// @route   GET api/suggestion
+// @desc    Get all suggestions by hostel id
+// @access  Public
+router.post('/hostel', [
+    check('hostel', 'Hostel is required').not().isEmpty()
+], getbyhostel);
+
+// @route   GET api/suggestion
+// @desc    Get all suggestions by student id
+// @access  Public
+router.post('/student', [
+    check('student', 'Student is required').not().isEmpty()
+], getbystudent);
+
+module.exports = router;
